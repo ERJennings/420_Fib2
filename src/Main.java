7bdafe1b051bad7c2b04f3ee094bd84a957b4838
ERJennings <erjennings13@gmail.com>
2020-12-09 03:20:24 -0500

    Code functional, needs comments and style

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Word");
+
+        int u = 0;
+        int v = 1;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a non-negative integer: ");
+
+        String enteredNumber = input.nextLine();
+        int n = Integer.parseInt(enteredNumber);
+
+        for (int i = 2; i < n; i++) {
+            v += u;
+            u = v - u;
+        }
+
+        if (n == 0) {
+            System.out.print("Fibonacci number " + n + " is 0");
+        }
+        else {
+            System.out.print("Fibonacci number " + n + " is " + v);
+        }
+
     }
 
 }
